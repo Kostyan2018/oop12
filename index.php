a2c103557f04d3446ea0504d59722362d7ff1a2e
Тетяна Пироговська <rockrebel298@gmail.com>
2023-08-01 19:20:41 +0300

    commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$teacherList = [];
+
+class Teacher
+{
+  public function __construct(
+      private string $name,
+      private string $jobTitle,
+      private ?string $subject = null
+  ){}
+
+  public function setName($name): void
+  {
+      $this->name = $name;
+  }
+
+}
+$evgen = new Teacher('Evgen', 'decan');
+$nata = new Teacher('Nata', 'zavkaf', 'math');
+
+echo '<pre>';
+print_r($evgen);
+print_r($nata);
+
+class TeacherList {
+    public array $teacherList = [] ;
+    public function __construct ($teacherList){
+        $this->teacherList = [];
+    }
+
+}
+
